c287eb324044089a50b29bc97e4ec04abaff7553
Albert Devasia <albertdevasia2024@mca.ajce.in>
2023-03-04 12:56:19 +0530

    until for each

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products=[
+    {
+      name:'Iphone 11',
+      category:'Mobile',
+      description:'This is a good phone',
+      image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.theverge.com%2F2019%2F9%2F10%2F20857974%2Fapple-iphone-11-hands-on-features-photos-video-price-camera&psig=AOvVaw2El1bNdFbvYYWvG2mlVdcK&ust=1678000391543000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPCV09vcwf0CFQAAAAAdAAAAABAE'
+    },
+    {
+      name:'One Plus 7T',
+      category:'Mobile',
+      description:'This is a good phone',
+      image:'https://fdn2.gsmarena.com/vv/pics/oneplus/oneplus-7t-1.jpg'
+    },{
+      name:'Oppo 10X',
+      category:'Mobile',
+      description:'This is a good phone',
+      image:'https://www.91-img.com/gallery_images_uploads/a/5/a500826ac13c354e0251aa7da087547db4c5ac86.jpeg?tr=h-550,w-0,c-at_max'
+
+    },{
+      name:'MI Note 9 Pro',
+      category:'Mobile',
+      description:'This is a good phone',
+      image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThy3aDf5SN66nEh2Vl_H3c3_otdM7X77z437W223Fp&s'
+
+    }
+  ]
+  res.render('index', {products});
+});
+
+module.exports = router;
